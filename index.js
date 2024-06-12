750af1fc977b29ff7ae5d4763f5931d3504162ce
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-06-12 20:57:38 +0200

    upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import { parse } from 'node-html-parser';
+
+const webhook = "
+const url = ""
+let lastVisits = 0;
+
+async function visitas() {
+    try {
+        const response = await fetch();
+        const html = await response.text();
+
+        const root = parse(html);
+        const element = root.querySelector('#countermatomo');
+        const visits = parseInt(element.innerText.trim().replace('Visites:', '').trim());
+
+  
+        if (visits > lastVisits) {
+            await webhook(visits, lastVisits);
+        }
+        lastVisits = visits;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function webhook(visitas, old) {
+    try {
+        const response = await fetch(url, {
+            method: 'post',
+            body: JSON.stringify({content: "@everyone Cambió de "+ visitas + " a "+ old }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        const data = await response.json();
+    } catch (error) {
+        
+    }
+}
+
+setInterval(visitas(), 86400000); 
